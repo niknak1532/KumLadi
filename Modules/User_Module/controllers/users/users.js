a105489c0fe6ad5c651d5bcf7f901f10f5c61c0a
Kamogelo Tsipa <kamotsipa@gmail.com>
2017-06-26 23:09:56 +0200

    Search via student_number
    Added function to get user details via student number

--- Modules/User_Module/controllers/users/users.js
+++ Modules/User_Module/controllers/users/users.js
@@ -2,8 +2,6 @@ var _       = require('lodash');
 var debug   = require('debug')('kumladi-api:controllers:users');
 var Users     = require('../../models/user');
 
-/*File for functional functions*/
-
 debug('Initialising users controller');
 
 debug('Exporting method: get');
@@ -57,3 +55,36 @@ module.exports.get = function(req, res, next){
   	 });
 };
 
+debug('Exporting method: getBySN');
+module.exports.getBySN = function(req, res, next){
+debug('Extracting user student_number from params');
+var sn = req.params.student_number;
+
+  	 debug('Trying to find user with student_number: ' + sn);
+	 Users.findOne({'student_number': sn}, function(err, user){
+     	 debug('Checking for errors');
+		 if(err) return  next(err);
+		 if(!user) return next(new Error('User not found'));
+
+    	 debug('Building JSON:API response');
+    	 var response = {
+       	     data: {
+             	 type: 'users',
+       			 id: user.id,
+       			 attributes: {
+           		 surname: user.surname,
+	   			 initials: user.initials,
+           		 student_number: user.student_number,
+           		 email: user.email,
+           		 user_level: user.user_level,
+           		 modules: user.modules,
+           		 subscribed_threads: user.subscribed_threads,
+           		 admin: user.admin
+        		}
+     	   }  
+    	};
+
+     	 debug('Sending response (status: 200)');
+     	 res.status(200).send(response);
+    });
+};
