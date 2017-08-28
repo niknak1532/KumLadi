7c64e06306c1312b8b30524d7cb9f9b3a4ee2262
Kamogelo Tsipa <kamotsipa@gmail.com>
2017-08-28 18:46:35 +0200

    Delete users.js

--- Modules/User_Module/controllers/users/users.js
+++
@@ -1,5 +0,0 @@
-var _       = require('lodash');
-var debug   = require('debug')('kumladi-api:controllers:users');
-var Users     = require('../../models/user');
-
-debug('Initialising the user controller')
